d9392e2330c72af81b9d7b95d183a3b24d0fac55
Esha <shuminnahar8di@gmail.com>
2026-06-07 08:00:35 +0600

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+import path from "node:path"
+
+
+dotenv.config({
+    path: path.resolve(process.cwd(), ".env")
+})
+
+
+const config = {
+    port: Number(process.env.PORT),
+    connectionString : process.env.CONNECTION_STRING
+}
+
+
+export default config;
