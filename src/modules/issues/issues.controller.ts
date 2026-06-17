7f4f65a39a0bcdd81328d206ebc0af94592aeee0
Esha <shuminnahar8di@gmail.com>
2026-06-17 08:17:04 +0600

    update issue added

---
+++ src/modules/issues/issues.controller.ts
@@ -0,0 +1,119 @@
+import { type Request, type Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import config from "../../config";
+import { issueService } from "./issues.service";
+import { pool } from "../../db";
+
+const createIssue = async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.authorization;
+    const decodedToken = jwt.verify(
+      token as string,
+      config.secret as string,
+    ) as JwtPayload;
+    const user = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      decodedToken.id,
+    ]);
+    const reporter_id = user.rows[0].id;
+    const { title, description, type } = req.body;
+    const result = await issueService.createIssue(
+      { title, description, type },
+      reporter_id,
+    );
+    return res.status(201).json({
+      success: true,
+      message: "Issue created successfully",
+      data: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error instanceof Error ? error.message : "Unknown error",
+    });
+  }
+};
+
+const getIssue = async (req: Request, res: Response) => {
+  try {
+    const { sort, type, status } = req.query as {
+      sort?: string;
+      type?: string;
+      status?: string;
+    };
+    const result = await issueService.getIssues({ sort, type, status });
+    res.status(200).json({
+      success: true,
+      message: "Issues retrived successfully",
+      data: result.rows,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error,
+    });
+  }
+};
+
+const getIssuebyId = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await issueService.getIssuebyIdfromDB(id);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "issue not found!!" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "issue retrieve successfully",
+      data: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "issue does not exist",
+      error: error,
+    });
+  }
+};
+const updateIssuebyID = async(req: Request, res: Response)=>{
+  const {id} =req.params;
+  const {title,description,type} = req.body;
+  try{
+    const result = await pool.query(
+      `
+      UPDATE 
+      issues set 
+      title = COALESCE($1, title), description = COALESCE($2,description ), type = COALESCE($3,type) where id = $4 RETURNING *
+  
+      `
+      ,[title,description,type,id]
+    )
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "issue not found!!" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "issue retrieve successfully",
+      data: result.rows[0],
+    });
+
+
+  }
+  catch(error){
+    res.status(500).json({
+      success: false,
+      message: "Issue does not exist",
+      error: error,
+    });
+
+  }
+
+}
+export const issueController = {
+  createIssue,
+  getIssue,
+  getIssuebyId,
+  updateIssuebyID
+};
--- src/modules/issues/issues.route.ts
+++ src/modules/issues/issues.route.ts
@@ -1,51 +1,12 @@
-import { Router, type Request, type Response } from "express";
+import { Router } from "express";
 import auth from "../../middleware/auth";
-import { pool } from "../../db";
-import config from "../../config";
-import jwt from "jsonwebtoken";
-import { type JwtPayload } from "jsonwebtoken";
+import { issueController } from "./issues.controller";
 
 export const router = Router();
-type status = "open" | "in_progress" | "resolved";
 
-router.post(
-  "/",
-  auth("contributor", "maintainer"),
-  async (req: Request, res: Response) => {
-    try {
-      const token = req.headers.authorization;
-
-      const decodedToken = jwt.verify(
-        token as string,
-        config.secret as string,
-      ) as JwtPayload;
-      const user = await pool.query(
-        `
-            SELECT * FROM users where id = $1`,
-        [decodedToken.id],
-      );
-      const reporter_id = user.rows[0].id;
-      const { title, description, type } = req.body;
-      const issue = await pool.query(
-        `
-            INSERT INTO issues (title, description, type,status,reporter_id) VALUES($1,$2,COALESCE($3,'bug'),'open',$4) RETURNING id,title,description,type,status,reporter_id,created_at,updated_at`,
-        [title, description, type, reporter_id],
-      );
-      return res.status(201).json({
-        success: true,
-        message: "Issue created successfully",
-        data: issue.rows[0],
-      });
-    } catch (error) {
-      console.error(error);
-
-      res.status(500).json({
-        success: false,
-        message: error instanceof Error ? error.message : "Unknown error",
-      });
-    }
-  },
-);
-
+router.post("/", auth("contributor", "maintainer"), issueController.createIssue);
+router.get("/", issueController.getIssue);
+router.get("/:id", issueController.getIssuebyId);
+router.put("/:id",auth("contributor", "maintainer"),issueController.updateIssuebyID)
 const issueRouter = router;
 export default issueRouter;
